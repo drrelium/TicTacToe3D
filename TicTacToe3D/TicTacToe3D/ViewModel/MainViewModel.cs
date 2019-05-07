3ecab0490f2197d5fcf90240e0e0052389da700a
drrelium <arielpurkey@gmail.com>
2019-05-06 20:15:53 -0700

    Attempted to add property change for color.

--- TicTacToe3D/TicTacToe3D/ViewModel/MainViewModel.cs
+++ TicTacToe3D/TicTacToe3D/ViewModel/MainViewModel.cs
@@ -14,10 +14,32 @@ namespace TicTacToe3D
     {
 
         public ICommand RingSelectionCommand{ set; get; }
+        public ICommand DisplaySelectionCommand { set; get; }
         public ICommand PaintCommand { get; set; }
         public ObservableCollection<CanvasRing> RingList { get; set; }
-        public ObservableCollection<CanvasRing> CanvasList { get; set; }
         public int BoardWidth = 3;
+        public SKColor largeColor;
+        public int selectedRingSize;
+        public SKColor currentPlayerColor;
+        public CanvasRing currentCanvasRing;
+
+        private ObservableCollection<CanvasRing> canvasList;
+
+        public ObservableCollection<CanvasRing> CanvasList
+        {
+            get { return canvasList; }
+            set
+            {
+                if (canvasList != value)
+                {
+                    canvasList = value;
+                    OnPropertyChanged("CanvasList");
+                }
+            }
+        }
+
+    
+
 
         public MainViewModel()
         {
@@ -26,38 +48,65 @@ namespace TicTacToe3D
             NewList(CanvasList, BoardWidth * BoardWidth);
             NewList(RingList, BoardWidth);
 
-            RingSelectionCommand = new Command<object>(UpdateRing);
+            RingSelectionCommand = new Command<object>(UpdateSelectedRing);
+            DisplaySelectionCommand = new Command<object>(UpdateRing);
             PaintCommand = new Command<SKPaintSurfaceEventArgs>(OnPainting);
+            currentPlayerColor = Color.Yellow.ToSKColor();
+        }
+
+        public void UpdateSelectedRing(object o)
+        {
+            var item = (o as CanvasRing);
+            int selectedRingSize = item.Location;
+            //         Debug.WriteLine("Clicked location " + location.ToString());
         }
 
         public void UpdateRing(object o)
         {
-            var item = (o as CanvasRing);
-            int location = item.Location;
+            var currentCanvasRing = (o as CanvasRing);
+            int location = currentCanvasRing.Location;
             //         Debug.WriteLine("Clicked location " + location.ToString());
+            
+            switch (selectedRingSize)
+            {
+                case 0:
+                    currentCanvasRing.SmallColor = currentPlayerColor;
+                    return;
+                case 1:
+                    currentCanvasRing.MediumColor = currentPlayerColor;
+                    return;
+                case 2:
+                    currentCanvasRing.LargeColor = currentPlayerColor;
+                    return;
+                default:
+                    Debug.WriteLine("Error: A ring size needs to be selected");
+                    return;
+            }
         }
+
 
         private void OnPainting(SKPaintSurfaceEventArgs e)
         {
-            Debug.WriteLine("Entered OnPainting in MV");
+            Debug.WriteLine("LargeColor is " + this.ToString());           
+       //     Debug.WriteLine("location= " + location);
             SKImageInfo info = e.Info;
             SKSurface surface = e.Surface;
             SKCanvas canvas = surface.Canvas;
+ //               this.LargeColor = LargeColor;
+
+        
+
 
             SKPaint LargeRing = new SKPaint
             {
                 Style = SKPaintStyle.Stroke,
-
-                //       Color = currentPlayerColor.WithAlpha(CanvasRings.LargeVisible),
+                Color = LargeColor,
                 StrokeWidth = (float)(info.Width * 0.10)
             };
 
             float LargeRingSize = (float)(info.Width * 0.80);
 
             canvas.DrawCircle(info.Width / 2, info.Height / 2, LargeRingSize / 2, LargeRing);
-
-
-
         }
 
         public void NewList(ObservableCollection<CanvasRing> list, int size) 
@@ -68,7 +117,7 @@ namespace TicTacToe3D
                 {
                     SmallColor = Color.Transparent.ToSKColor(),
                     MediumColor = Color.Transparent.ToSKColor(),
-                    LargeColor = Color.Transparent.ToSKColor(),
+                    LargeColor = Color.Blue.ToSKColor(),
                     Location = x
                 });
 
@@ -76,24 +125,24 @@ namespace TicTacToe3D
         }
 
 #region properties  
-public byte SmallVisible
-        {
-            get { return smallVisible; }
-            set
-            {
-                smallVisible = value;
-                OnPropertyChanged("SmallVisible");
-            }
-        }
 
-        public SKColor SmallColor
+        public SKColor LargeColor
         {
-            get { return smallColor; }
-            set
-            {
-                smallColor = Color.Yellow.ToSKColor();
-                OnPropertyChanged("SmallColor");
-            }
+                get
+                {
+                if (currentCanvasRing == null)
+                    return Color.Transparent.ToSKColor();
+                else
+                    return currentCanvasRing.LargeColor;
+                }
+                set
+                {
+                    if (currentCanvasRing.LargeColor != value)
+                        currentCanvasRing.LargeColor = value;
+                    Debug.WriteLine("LargeColor= " + LargeColor);
+                    OnPropertyChanged("LargeColor");
+                }
+
         }
         #endregion
     }
